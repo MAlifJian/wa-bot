2105f91790da29968cf42f6a4ff856725e7bcaad
MAlifJian <81363642+MAlifJian@users.noreply.github.com>
2021-10-23 11:43:31 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ const ytdl = require("ytdl-core");
 ffmpeg.setFfmpegPath(ffmpegPath);
 //Function Import
 const {fetcher, getBuffer} = require('./lib/fetcher.js')
+const {getRandom} = require('./lib/function.js')
 
+// Timer
+const date = new Date();
+
+const startHours = date.getHours();
+const startMinutes = date.getMinutes();
+const startSecond = date.getSeconds();
+
+console.log(`${date.getTime()} ${startHours} Jam ${startMinutes} Menit`);
 // Database Config
 let ban = JSON.parse(fs.readFileSync("./dbs/banList.json"));
 
@@ -35,6 +44,13 @@ async function start(){
         console.log(`has new chats ${hasNewChats} chat:${alf.chats.get.name}`);
     })
     alf.on('chat-update', async (cht) => {
+        const nowDate = new Date();
+        const online = nowDate.getTime() - date.getTime();
+
+        const nowSecond = Math.floor((online % (1000 * 60))/ 1000);
+        const nowMinutes = Math.floor((online % (1000 * 60 * 60)) / (1000 * 60));
+        const nowHours = Math.floor((online % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const timeOnline = `${nowHours} Jam ${nowMinutes} Menit ${nowSecond} Detik`;
         try {
             if (!cht.hasNewMessage) return;
             let nama = '';
@@ -56,14 +72,44 @@ async function start(){
             const groupMembers = isGroup ? groupMetadata.participants : "";
             let isAdmin = isGroup ? await groupMembers.findIndex((f,i) => {if (f.jid === sender && f.isAdmin === true) return true}) : false;
             // console.log(JSON.parse(JSON.stringify(cht).replace('quotedM', 'm').message.extendedTextMessage.contextInfo));
-            console.log(isAdmin);
             isAdmin = isAdmin > 0 ? true : false;
             const isOwner = sender === '6289624835956@s.whatsapp.net';
             //Body message
             const type = Object.keys(cht.message)[0];
+            const ephemerallMsg = type === 'ephemeralMessage' ? 'TRUE' : 'FALSE';
+            const menu = `༺ *MENU ALF  BOT* ༻
+│──────BOT───────│
+Prefix: *.*
+Online: ${timeOnline}
+
+│──────Group──────│
+Name: *${groupMetadata.subject}*
+Members: *${groupMembers.length}*
+Ephemeral Message: *${ephemerallMsg}*
+
+│──────Menu──────│
+> *Admin*
+.kick
+.tagall
+.hidetag
+.infoall
+.add
+
+> *Media*
+.sticker
+.toimg
+.ytmp4
+.ytmp3
+
+> *Menu Ban*
+.ban
+.banlist
+.unban
+`
+
             const isQuoted = type === 'extendedTextMessage' ? true : false;
-            
-            body = (type === 'conversation' && cht.message.conversation) ? cht.message.conversation : (type == 'imageMessage') && cht.message.imageMessage.caption ? cht.message.imageMessage.caption : (type == 'videoMessage') && cht.message.videoMessage.caption ? cht.message.videoMessage.caption : (type == 'extendedTextMessage') && cht.message.extendedTextMessage.text ? cht.message.extendedTextMessage.text : '';
+            console.log(type);
+            body = (type === 'conversation' && cht.message.conversation) ? cht.message.conversation : (type == 'imageMessage') && cht.message.imageMessage.caption ? cht.message.imageMessage.caption : (type == 'videoMessage') && cht.message.videoMessage.caption ? cht.message.videoMessage.caption : (type == 'extendedTextMessage') && cht.message.extendedTextMessage.text ? cht.message.extendedTextMessage.text : (type == 'ephemeralMessage' ) ? cht.message.ephemeralMessage.message.extendedTextMessage.text : '';
             if (body === 'bot'){
                     texts = 
                     `────────────────────────\nOwner         : Alif Jian\nContact       : wa.me/+6289624835956\nDescription : Hubungi Owner Untuk Menu Baru,Bot Masih Dalam Pengembangan\n\n
@@ -74,6 +120,7 @@ async function start(){
             }
             const command = body.startsWith(prefix) ? body.slice(1).trim().split(" ")[0].toLowerCase() : '';
             console.log(`Perintah Dari Nama: ${nama}\nPerintah:${body}\nGroup: ${groupMetadata.subject}`);
+            if(isBanned) return;
             switch (command) {
             //About Bot
                 case 'info':
@@ -85,13 +132,12 @@ async function start(){
                     await alf.sendMessage(pengirim, texts, text,{quoted: cht});
                     break;
                 case 'menu':
-                    texts = `│༺*MENU ALF  BOT*༻\n┝──────BOT──────\n│\n*Prefix *.\n│\n↳.info\n│\n↳.menu\n┝─────Menu Ban─────\n│\n↳.banlist\n│\n↳.ban [tag] (only admins)\n┝─────Tag Menu─────\n│\n↳.tagall (only admin)\n│\n↳.infoall\n│\n↳.hidetag [text] (default) = (kosong)\n┝─────Media Menu─────\n│\n↳.sticker [reply image / caption gambar]\n│\n↳.ytdl [linkyt] (return mp4)`
-                    await alf.sendMessage(pengirim,texts,text);
+                    await alf.sendMessage(pengirim,menu,text);
                 break;
             //Tag Case
                 case 'tagall' :
                     if(!isGroup) return;
-                    if(!isAdmin) return;
+                    if(!isAdmin && !isOwner) return;
                     if(isBanned) return;
                     texts = '*ALF  BOT*\n';
                     members = [];
@@ -107,45 +153,44 @@ async function start(){
                     texts = '*INFOALL:*\n'
                 case 'hidetag':
                     if(!isGroup) return;
+                    if(!isAdmin && !isOwner) return;
                     if(isBanned) return;
-                    if(texts === "") texts = `${body.slice(9)}`;
+                    texts = `${body.slice(9)}`;
+                    console.log()
                     members = [];
                     groupMembers.forEach(m => {
                         members.push(m.jid);
                     })
-                    status = await alf.sendMessage(pengirim, texts ,extendedText, {contextInfo : {mentionedJid : members} })
-                    console.log("Pesan Hidetag Terkirim dengan ID " + status.key);
+                    await alf.sendMessage(pengirim, texts ,extendedText, {contextInfo : {mentionedJid : members} })
+                    console.log("Pesan Hidetag Terkirim dengan ID ");
                     break;
                 //Admin Case
                 case 'kick':
-                    if(!isGroup) return;
-                    if(isAdmin || isOwner){
-                        var target = `${body.slice(7)}`;
-                        await alf.sendMessage(pengirim, `Kata Kata Terakhirnya Bang @${target}`, extendedText, {contextInfo : {mentionedJid : [`${target}@s.whatsapp.net`]}})
-                        await alf.groupRemove(pengirim, [`${target}@s.whatsapp.net`]);
-                    }else{
-                        await alf.sendMessage(pengirim, `Hanya Untuk Admin`,text,{quoted: cht});
+                    if(!isGroup ) return;
+                    if(!isAdmin || !isOwner){
+                        return await alf.sendMessage(pengirim, `Hanya Untuk Admin`,text,{quoted: cht});
                     }
+                    var target = isQuoted ? JSON.parse(JSON.stringify(cht).replace('quotedM','m')).message.extendedTextMessage.contextInfo.participant : `${body.slice(7)}@s.whatsapp.net`;
+                    await alf.groupRemove(pengirim, [`${target}`]);
+                break;
                 case 'add':
                     if(!isGroup) return;
-                    if(isAdmin || isOwner){
-                        var target = isQuoted ? JSON.parse(JSON.stringify(cht).replace('quotedM','m')).message.extendedTextMessage.contextInfo : `${body.slice(7)}`;
-                        await alf.groupAdd(pengirim, [`${target}@s.whatsapp.net`]);
-                        await alf.sendMessage(pengirim, `Hiya Kena Culik`)
-                    }else{
-                        await alf.sendMessage(pengirim, `Hanya Untuk Admin`,text,{quoted: cht});
+                    if(!isAdmin || !isOwner){
+                        return await alf.sendMessage(pengirim, `Hanya Untuk Admin`,text,{quoted: cht});
                     }
-                break;
+                    var target = isQuoted ? JSON.parse(JSON.stringify(cht).replace('quotedM','m')).message.extendedTextMessage.contextInfo.participant : `${body.slice(7)}@s.whatsapp.net`;
+                    console.log(target)
+                    await alf.groupAdd(pengirim, [`${target}`]);
                 break;
                 // Ban Case
                 case 'ban' :
                     if(!isGroup) return;
-                    console.log(isAdmin)
                     if(isAdmin || isOwner){
-                        console.log(isAdmin);
-                        var number = isQuoted ? JSON.parse(JSON.stringify(cht).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo: `${body.slice(5).replace('@', '')}@s.whatsapp.net`;
-                        await ban.banList.push(number);
-                        fs.writeFileSync('./dbs/banList.json', JSON.stringify(ban));
+                        var number = body.slice(5) === ''? JSON.parse(JSON.stringify(cht).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo.participant : `${body.slice(5).replace('@', '')}@s.whatsapp.net`;
+                        if(!ban.banList.includes(number)){
+                            await ban.banList.push(number);
+                            fs.writeFileSync('./dbs/banList.json', JSON.stringify(ban));
+                        }
                         console.log(`Nomor TerBanned ${number}`);    
                     }else{
                         return await alf.sendMessage(pengirim, "Hanya Admin",text,{quoted: cht});;  
@@ -154,9 +199,8 @@ async function start(){
                 case 'unban' :
                     if(!isGroup) return;
                     if(!isAdmin) return await alf.sendMessage(pengirim, "Hanya Admin",text,{quoted: cht});
-                    var number = `${body.slice(7).replace('@', '')}@s.whatsapp.net`;
+                    var number = body.slice(5) === ''? JSON.parse(JSON.stringify(cht).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo.participant : `${body.slice(5).replace('@', '')}@s.whatsapp.net`;
                     number = await ban.banList.findIndex((f) => f === number);
-                    if(number === -1) return await alf.sendMessage(pengirim, "Tag Setelah Perintah\nContoh:\n.unban @orangnya",text,{quoted: cht})
                     ban.banList.splice(number,1);
                     fs.writeFileSync('./dbs/banList.json', JSON.stringify(ban));
                 break;
@@ -178,34 +222,36 @@ async function start(){
                         var url = await `${body.slice(7)}`;
                         var info = await ytdl.getInfo(url);
                         var format = await ytdl.chooseFormat(info.formats, {quality : '18'});
-                        await alf.sendMessage(pengirim, '⏳Tunggu Sedang Di Proses', extendedText, {quoted : cht});
+                        await alf.sendMessage(pengirim, `⏳Tunggu Sedang Di Proses`, extendedText, {quoted : cht});
                         buffer = await getBuffer(format.url);
-                        await alf.sendMessage(pengirim, buffer, video, {quoted : cht,mimetype : 'video/mp4',filename : `${info.videoDetails.title}.mp4`,caption : `Title : ${info.videoDetails.title}\n\nChannel: ${info.videoDetails.author.name}\n\nUploaded: ${info.videoDetails.uploadDate}\n\nDownload Completed ✅`});
-                    }catch{
+                        await alf.sendMessage(pengirim, buffer, video, {quoted : cht,mimetype : 'video/mp4',filename : `${info.videoDetails.title}.mp4`,thumbnail: fs.readFileSync('./thumb.jpeg'),caption : `Title : ${info.videoDetails.title}\n\nChannel: ${info.videoDetails.author.name}\n\nUploaded: ${info.videoDetails.uploadDate}\n\nDownload Completed ✅`});
+                    }catch(err){
+                        console.log(err);
                         await alf.sendMessage(pengirim, '❌ Ada Error Coba Contact Owner', extendedText, { quoted : cht})
                     }
                     break;
                 case 'ytmp3':
                     // if(!isGroup) return await alf.sendMessage(pengirim, 'Hanya Di Group', extendedText, {quoted : cht});
+                    var url = await body.slice(7);
                     try{
-                        var url = await body.slice(7);
                         await alf.sendMessage(pengirim, '⏳Tunggu Sedang Di Proses', extendedText, {quoted : cht});
-                        var urlM = await fetcher(`http://zekais-api.herokuapp.com/ytmp3?url=${url}`,{method : 'get'});
+                        var urlM = await fetcher(`https://michaelbelgium.me/ytconverter/convert.php?youtubelink=${url}`,{method : 'get'});
                         console.log(urlM);
-                        infomp3 = `╭─「 *TIMELINE PLAY MP3* 」\n│ *• Judul:* ${urlM.title}\n│ *• Channel:* ${urlM.channel}\n│ *•Size:* ${urlM.size}\n│ *•Link:* ${urlM.result}\n│\n│ *TUNGGU SEBENTAR LAGI DIKIRIM*\n│ *MOHON JANGAN SPAM YA BEB*\n╰─────────────────────`
-                        bufferG = await getBuffer(urlM.thumb);
-                        buffer = await getBuffer(urlM.result);
-                        console.log('Sudah Terdownload')
-                        await alf.sendMessage(pengirim, bufferG, image, {quoted : cht,caption : infomp3});
-                        await alf.sendMessage(pengirim, buffer, audio, {mimetype : 'audio/mp4',quoted : cht,filename : `lagu.mp3`,ptt : true});
+                        infomp3 = `╭─「 *TIMELINE PLAY MP3* 」\n│ *• Judul:* ${urlM.title}\n│ *•Link:* ${urlM.file}\n│\n│ *TUNGGU SEBENTAR LAGI DIKIRIM*\n│ *MOHON JANGAN SPAM YA BEB*\n╰─────────────────────`
+                        buffer = await getBuffer(urlM.file);
+                        console.log(buffer)
+                        await alf.sendMessage(pengirim, fs.readFileSync('thumb.jpeg'), image, {quoted : cht,caption : infomp3});
+                        await alf.sendMessage(pengirim, buffer, audio, {mimetype : 'audio/mp4',quoted : cht,filename : `${urlM.title}.mp3`,ptt : true});
                     }catch(err){
                         console.log(err)
                         await alf.sendMessage(pengirim, '❌ Ada Error Coba Contact Owner', extendedText, { quoted : cht})
                     }
                 break;
+                case "stiker":
                 case 'sticker':
-                    const encmedia = isQuoted ? JSON.parse(JSON.stringify(cht).replace('quotedM','m')).message.extendedTextMessage.contextInfo : cht;
+                    var encmedia = isQuoted ? JSON.parse(JSON.stringify(cht).replace('quotedM','m')).message.extendedTextMessage.contextInfo : cht;
                     var gambar = await alf.downloadAndSaveMediaMessage(encmedia);
+                    var namaGambar = getRandom('.webp');
                     await alf.sendMessage(pengirim, '⏳Tunggu Sedang Di Proses', extendedText, {quoted : cht});
                     await ffmpeg(`./${gambar}`)
                         .input(gambar)
@@ -214,13 +260,34 @@ async function start(){
                         })
                         .on('end',async () => {
                             console.log('Selesai Membuat Sticker');
-                            await alf.sendMessage(pengirim, fs.readFileSync('./cobaa.webp'), sticker, {quoted : cht});
-                            fs.unlinkSync('./cobaa.webp');
+                            await alf.sendMessage(pengirim, fs.readFileSync(namaGambar), sticker, {quoted : cht});
+                            fs.unlinkSync(namaGambar);
                             fs.unlinkSync(gambar);
                         })
                         .addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
                         .toFormat('webp')
-                        .save('cobaa.webp')
+                        .save(namaGambar)
+                break;
+                case'toimg':
+                case 'image':
+                    var encmedia = JSON.parse(JSON.stringify(cht).replace('quotedM','m')).message.extendedTextMessage.contextInfo;
+                    var gambar = await alf.downloadAndSaveMediaMessage(encmedia);
+                    var namaGambar = getRandom('.png');
+                    await alf.sendMessage(pengirim, '⏳Tunggu Sedang Di Proses', extendedText, {quoted : cht});
+                    await ffmpeg(`./${gambar}`)
+                        .input(gambar)
+                        .on('error', err =>{
+                            console.log(`Error : ${err}`);
+                        })
+                        .on('end',async () => {
+                            var buffGambar = await fs.readFileSync(namaGambar);
+                            console.log('Selesai Membuat Gambar');
+                            await alf.sendMessage(pengirim, buffGambar, image, {quoted : cht});
+                            fs.unlinkSync(namaGambar);
+                            fs.unlinkSync(gambar);
+                        })
+                        .output(namaGambar)
+                        .run();
                 break;
                 default:
                     break;
